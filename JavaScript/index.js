b80035bee2927b8624574292e2c6aba6b9318ba1
Mohammed Khader <s.mohammed.khader@accenture.com>
2023-07-25 19:18:17 +0530

    basic topics1

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,7 @@
-var nameN = "Seema"
+var nameN = "seema"
 console.log("My name is "+nameN)
 console.log(nameN.length)
+console.log(nameN.slice(1,2))
+console.log(nameN.toUpperCase())
+console.log(nameN.toLowerCase())
+console.log(nameN.slice(0,1,nameN.toUpperCase).toLowerCase())
