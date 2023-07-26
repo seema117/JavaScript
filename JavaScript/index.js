6144ef76351cf080aad47b2737d0e39929263bfe
Mohammed Khader <s.mohammed.khader@accenture.com>
2023-07-26 23:16:44 +0530

    basic topics2

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,7 +1,31 @@
-var nameN = "seema"
+/*var nameN = "seema"
 console.log("My name is "+nameN)
 console.log(nameN.length)
 console.log(nameN.slice(1,2))
 console.log(nameN.toUpperCase())
-console.log(nameN.toLowerCase())
-console.log(nameN.slice(0,1,nameN.toUpperCase).toLowerCase())
+console.log(nameN.toLowerCase())*/
+function getMilk(moneyGiven) { 
+    var c =moneyGiven/2;
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("Take "+ c +" of Milk");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+getMilk(moneyGiven)
+
+var moneyGiven = 10
